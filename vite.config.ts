19f34b3a0c0e63b1b0ce5480a9cdc6d83145b3d1
AlimaTABS <79248340+AlimaTABS@users.noreply.github.com>
2026-01-07 21:52:09 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './My-Helper',
+  base: '/My-Helper/'
   define: {
     // This makes process.env.API_KEY available in your code
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
